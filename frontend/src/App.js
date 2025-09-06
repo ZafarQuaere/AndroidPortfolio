8a8fd3f9626b8bd26dbb106a9f13a371fbfb391c
zafar.imam <zafar.imam@lalamove.com>
2025-09-06 23:52:51 +0530

    fix: add basename to BrowserRouter for GitHub Pages compatibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import { Toaster } from "./components/ui/toaster";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/AndroidPortfolio">
         <Routes>
           <Route path="/" element={<PortfolioPage />} />
         </Routes>
